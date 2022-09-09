b498c858154f7950964fe9b5b140f53798f84bee
davidhardin <thisgoestodavid@gmail.com>
2022-09-09 12:43:03 -0600

    initial

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+function Header() {
+    return (
+    <header class="headerflex">
+        <span>
+            <img src="me.jpg" alt="img"/>
+        </span>
+        <span id="span-h">
+            david hardin
+        </span>
+    
+    
+    <div id="hbox">
+        <span>
+            <a href="#section-about">about</a>
+        </span>
+
+        <span>
+            <a href="#section-work">work</a>
+        </span>
+
+        <span>
+            <a href="#section-contact">contact</a>
+        </span>
+    </div>
+</header>
+    )
+}
+
+export default Header
